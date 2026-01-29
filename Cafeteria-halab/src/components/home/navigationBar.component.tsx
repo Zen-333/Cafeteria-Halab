1a5852ed890be95ace2c96b89579521570e142eb
Zen-333 <zenhamam56@gmail.com>
2026-01-29 16:37:57 +0000

    nav bar phone optimization

--- Cafeteria-halab/src/components/home/navigationBar.component.tsx
+++ Cafeteria-halab/src/components/home/navigationBar.component.tsx
@@ -1,4 +1,5 @@
 import '../../styles/home/navigationBar.css'
+import { useState } from "react"
 import { Link } from 'react-router-dom'
 import HalabLogo from "../../assets/Images/Halab-Logo.png"
 
@@ -7,47 +8,55 @@ import { faBook, faBars, faAddressBook } from '@fortawesome/free-solid-svg-icons
 import { faFacebookF, faInstagram, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 function NavigationBarComponent() {
+
+  const [menuOpen, setMenuOpen] = useState(false)
+
   return (
-    <div>
-      <div className="container">
-        <nav className="content">
-          <Link to="/">
-            <img className="logo" src={HalabLogo} alt="Halab Logo"/>
-          </Link>
-          <div className="nav-links">
+    <div className="container">
+      <nav className="content">
 
-            <div className="nav-socials">
-              <a href="#" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faFacebookF} />
-              </a>
-              <a href="#" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faInstagram} />
-              </a>
-              <a href="#" target="_blank" rel="noreferrer">
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-            </div>
+        <Link to="/">
+          <img className="logo" src={HalabLogo} alt="Halab Logo"/>
+        </Link>
 
-            <ul id="sidemenu">
-              <li>
-                <FontAwesomeIcon icon={faBook} style={{ color: "#ceab60" }} />
-                <Link to="/ourStory">Our Story</Link>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faBars} style={{ color: "#ceab60" }} />
-                <Link to="/menu">Menu</Link>
-              </li>
-              <li>
-                <FontAwesomeIcon icon={faAddressBook} style={{ color: "#ceab60" }} />
-                <Link to="/contact">Contact</Link>
-              </li>
-            </ul>
+        {/* Hamburger */}
+        <div 
+          className="hamburger"
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          <FontAwesomeIcon icon={faBars} />
+        </div>
 
+        <div className={`nav-links ${menuOpen ? "active" : ""}`}>
+          <div className="nav-socials">
+            <a href="#"><FontAwesomeIcon icon={faFacebookF} /></a>
+            <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
+            <a href="#"><FontAwesomeIcon icon={faTwitter} /></a>
           </div>
-        </nav>
-      </div>
+          
+          <ul id="sidemenu">
+            <li onClick={() => setMenuOpen(false)}>
+              <FontAwesomeIcon icon={faBook} style={{ color: "#ceab60" }} />
+              <Link to="/ourStory">Our Story</Link>
+            </li>
+
+            <li onClick={() => setMenuOpen(false)}>
+              <FontAwesomeIcon icon={faBars} style={{ color: "#ceab60" }} />
+              <Link to="/menu">Menu</Link>
+            </li>
+
+            <li onClick={() => setMenuOpen(false)}>
+              <FontAwesomeIcon icon={faAddressBook} style={{ color: "#ceab60" }} />
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+
+
+        </div>
+      </nav>
     </div>
   )
 }
+
 
 export default NavigationBarComponent
