951c8687bca5dcbd5dc483b4161ae81ae7110a95
Zen-333 <zenhamam56@gmail.com>
2026-01-29 16:29:26 +0000

    scroll to top component

--- Cafeteria-halab/src/App.tsx
+++ Cafeteria-halab/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import NavigationBarComponent from "./components/home/navigationBar.component.tsx"
 import Footer from "./components/home/footer.component.tsx"
+import ScrollToTop from "./components/layout/scrollToTop.tsx"
 
 import HomePage from "./pages/Home.page.tsx"
 import OurStoryPage from "./pages/OurStory.page.tsx"
@@ -13,6 +14,7 @@ function App() {
   return (
     <>
       <NavigationBarComponent />
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ourStory" element={<OurStoryPage />} />
